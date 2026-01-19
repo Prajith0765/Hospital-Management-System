587a4635859df7f7e1a10b28f119adc268c2353c
Prajith P <prajith.ps@UnitecareSoftwareSolutionsP.onmicrosoft.com>
2026-01-19 14:44:58 +0530

    Add project files.

---
+++ HospitalManagement/Entity/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Entity
+{
+    internal class Doctor
+    {
+
+        public int doctorId;
+        public string doctorName;
+        public string doctorType;
+        public byte experience;
+
+        public Doctor()
+        {
+
+        }
+    }
+}
---
+++ HospitalManagement/Entity/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Entity
+{
+    internal class Patient
+    {
+
+        public string name;
+        public int num = 1;
+        public int patientId;
+        public byte age;
+        
+
+        public Patient(string name, byte age)
+        {
+            this.name = name;
+            this.patientId = num++;
+            this.age = age;
+
+        }
+    }
+}
